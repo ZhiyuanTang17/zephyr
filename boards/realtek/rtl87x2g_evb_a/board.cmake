3d8ab6e114e71cfad75546f51ba22b82ee002c4e
Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>
2026-04-08 14:21:37 +0800

    boards: realtek: rtl87x2g_evb_a: fix jlink flash auto reset issue
    Use --reset-after-load instead of --reset to ensure the device
    automatically resets after flashing with west flash.
    
    Signed-off-by: Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>

--- boards/realtek/rtl87x2g_evb_a/board.cmake
+++ boards/realtek/rtl87x2g_evb_a/board.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) 2026, Realtek Semiconductor Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=RTL87X2G" "--speed=4000")
+board_runner_args(jlink "--device=RTL87X2G" "--speed=4000" "--reset-after-load" "--reset-type=0")
 
 include(${ZEPHYR_BASE}/boards/common/mpcli.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
