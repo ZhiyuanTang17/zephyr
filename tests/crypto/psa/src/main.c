0305ef4615a12edb6d47f527ed69885e336b33d4
Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>
2026-08-12 15:06:09 +0800

    tests: crypto: psa: add ccm and timing statistics
    Signed-off-by: Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>

--- tests/crypto/psa/src/main.c
+++ tests/crypto/psa/src/main.c
@@ -10,6 +10,7 @@
  */
 
 #include <zephyr/ztest.h>
+#include <zephyr/timing/timing.h>
 
 #include <psa/crypto.h>
 
@@ -165,6 +166,10 @@ ZTEST_USER(psa_crypto_test_suite, test_aes_ecb)
 		0xea, 0x5e, 0x61, 0xae, 0x81, 0x67, 0xca, 0xa0,
 		0x58, 0x63, 0x88, 0xeb, 0x9a, 0x7c, 0xb7, 0x55
 	};
+	timing_t start;
+	timing_t end;
+	uint64_t cycles;
+	uint64_t nanoseconds;
 	size_t out_len;
 	psa_status_t status;
 
@@ -176,11 +181,94 @@ ZTEST_USER(psa_crypto_test_suite, test_aes_ecb)
 	status = psa_import_key(&key_attr, key, sizeof(key), &key_id);
 	zassert_equal(status, PSA_SUCCESS);
 
+	timing_init();
+	timing_start();
+	start = timing_counter_get();
 	status = psa_cipher_encrypt(key_id, PSA_ALG_ECB_NO_PADDING, in_buf, sizeof(in_buf),
 				    out_buf, sizeof(out_buf), &out_len);
+	end = timing_counter_get();
+	cycles = timing_cycles_get(&start, &end);
+	nanoseconds = timing_cycles_to_ns(cycles);
+	TC_PRINT("AES-ECB encrypt time: %llu ns (%llu cycles)\n",
+		 (unsigned long long)nanoseconds, (unsigned long long)cycles);
 	zassert_equal(status, PSA_SUCCESS);
 
 	zassert_mem_equal(out_buf, out_buf_ref, sizeof(out_buf_ref));
+
+	status = psa_destroy_key(key_id);
+	zassert_equal(status, PSA_SUCCESS);
+}
+
+ZTEST_USER(psa_crypto_test_suite, test_aes_ccm)
+{
+	static const uint8_t key[] = {
+		0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7,
+		0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf
+	};
+	static const uint8_t nonce[] = {
+		0x00, 0x00, 0x00, 0x03, 0x02, 0x01, 0x00,
+		0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5
+	};
+	static const uint8_t additional_data[] = {
+		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07
+	};
+	static const uint8_t plaintext[] = {
+		0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+		0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+		0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e
+	};
+	static const uint8_t ciphertext_ref[] = {
+		0x58, 0x8c, 0x97, 0x9a, 0x61, 0xc6, 0x63, 0xd2,
+		0xf0, 0x66, 0xd0, 0xc2, 0xc0, 0xf9, 0x89, 0x80,
+		0x6d, 0x5f, 0x6b, 0x61, 0xda, 0xc3, 0x84, 0x17,
+		0xe8, 0xd1, 0x2c, 0xfd, 0xf9, 0x26, 0xe0
+	};
+	psa_key_attributes_t key_attr = PSA_KEY_ATTRIBUTES_INIT;
+	psa_algorithm_t algorithm = PSA_ALG_AEAD_WITH_SHORTENED_TAG(PSA_ALG_CCM, 8);
+	psa_key_id_t key_id = PSA_KEY_ID_NULL;
+	uint8_t ciphertext[sizeof(ciphertext_ref)] = {0};
+	uint8_t decrypted[sizeof(plaintext)] = {0};
+	timing_t start;
+	timing_t end;
+	uint64_t cycles;
+	uint64_t nanoseconds;
+	size_t output_length;
+	psa_status_t status;
+
+	psa_set_key_type(&key_attr, PSA_KEY_TYPE_AES);
+	psa_set_key_algorithm(&key_attr, algorithm);
+	psa_set_key_usage_flags(&key_attr, PSA_KEY_USAGE_ENCRYPT | PSA_KEY_USAGE_DECRYPT);
+
+	status = psa_import_key(&key_attr, key, sizeof(key), &key_id);
+	zassert_equal(status, PSA_SUCCESS);
+
+	timing_init();
+	timing_start();
+	start = timing_counter_get();
+	status = psa_aead_encrypt(key_id, algorithm, nonce, sizeof(nonce), additional_data,
+				  sizeof(additional_data), plaintext, sizeof(plaintext), ciphertext,
+				  sizeof(ciphertext), &output_length);
+	end = timing_counter_get();
+	cycles = timing_cycles_get(&start, &end);
+	nanoseconds = timing_cycles_to_ns(cycles);
+	TC_PRINT("AES-CCM encrypt time: %llu ns (%llu cycles)\n",
+		 (unsigned long long)nanoseconds, (unsigned long long)cycles);
+	zassert_equal(status, PSA_SUCCESS);
+	zassert_equal(output_length, sizeof(ciphertext_ref));
+	zassert_mem_equal(ciphertext, ciphertext_ref, sizeof(ciphertext_ref));
+
+	start = timing_counter_get();
+	status = psa_aead_decrypt(key_id, algorithm, nonce, sizeof(nonce), additional_data,
+				  sizeof(additional_data), ciphertext, sizeof(ciphertext), decrypted,
+				  sizeof(decrypted), &output_length);
+	end = timing_counter_get();
+	cycles = timing_cycles_get(&start, &end);
+	nanoseconds = timing_cycles_to_ns(cycles);
+	TC_PRINT("AES-CCM decrypt time: %llu ns (%llu cycles)\n",
+		 (unsigned long long)nanoseconds, (unsigned long long)cycles);
+	zassert_equal(status, PSA_SUCCESS);
+	zassert_equal(output_length, sizeof(plaintext));
+	zassert_mem_equal(decrypted, plaintext, sizeof(plaintext));
 
 	status = psa_destroy_key(key_id);
 	zassert_equal(status, PSA_SUCCESS);
